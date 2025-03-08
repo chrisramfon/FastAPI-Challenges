84dfa8b75405c839ba5122906de99347ae5fbadd
chrisramfon <chrisramfon@outlook.com>
2025-03-08 11:37:40 -0600

    [UPDATE] app.py This commit adds a better example.

--- Challenge-08/app.py
+++ Challenge-08/app.py
@@ -1,7 +1,6 @@
 def bubble_sorting( data: list ):
 
     did_sort = True
-
     list_len = len( data )
 
     while( did_sort ):
@@ -23,9 +22,25 @@ def bubble_sorting( data: list ):
     return data
 
 
+def bubble_sorting_two( data ):
+
+    array_len = len( data )
+
+    for index1 in range( array_len ):
+
+        for index2 in range( array_len - index1 - 1 ):
+
+            if data[ index2 ] > data[ index2 + 1]:
+                temp = data[ index2 ]
+                data[ index2 ] = data[ index2 + 1 ]
+                data[ index2 + 1 ] = temp
+
+    return data
+
+
 list1 = [ 1, 5, 9, 6 ]
 list2 = [ 9, 8, 7, 1, 10 ]
 list3 = [ 7, 8, 5, 9, 3, 2, 2, 1, 4, 6 ]
-print( bubble_sorting( list1 ) )
-print( bubble_sorting( list2 ) )
-print( bubble_sorting( list3 ) )
+print( bubble_sorting_two( list1 ) )
+print( bubble_sorting_two( list2 ) )
+print( bubble_sorting_two( list3 ) )
